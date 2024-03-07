477eca928beb04496862628f926d12d44d80f0bc
Pascall <Pascall@HOME-PC>
2024-03-08 00:00:12 +0300

    PASCALTRIAGLE

--- PASCALTRIAGLE/main.cpp
+++ PASCALTRIAGLE/main.cpp
@@ -1,7 +1,12 @@
 //PASCALTRIAGLE
 #include<iostream>
 using namespace std;
+
 const int width = 8;
+
+typedef unsignet long long int DataType;        //Директива 'typedef'создает псевдоним для существующего типа данных
+//typedef существующий_тип_данных новое_имя
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -14,22 +19,23 @@ void main()
 	}
 	cout << left;
 	cout << 1 << endl;
-	int nf = 1;
+	DataType nf = 1;
 	for (int n = 1; n <= h; n++)
 	{
 		nf *= n;
-		int mf = 1;
+		DataType mf = 1;
 		for (int i = 0; i < h - n; i++)
 		{
-			cout.width(width / 2); 
-			cout << "";
+			cout.width(width / 2);   //  задает ширину вывода на экран,а точнее,определяет,сколько знакопозиций(бит) будет занимать выводимое значение
+		    //если выводимое значение занимает больше знакопозиций,то оно выйдет за пределы указанной ширины.
+			cout << "";// Даже при выводе пустой строки,она займет всю заданную ширину(width/2)
 		}
 		cout.width(width);
 		cout << 1;
 		for (int m = 1; m <= n; m++)
 		{
 			mf *= m;
-			int nmf = 1;
+			DataType nmf = 1;
 			for (int nm = 1; nm <= n - m; nm++)nmf *= nm;
 			cout.width(width);
 			cout << nf / mf / nmf;
